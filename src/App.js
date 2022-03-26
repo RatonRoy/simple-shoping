dcda72054421d7298e78c1bfdc9d5f67a8b1865e
RatonRoy <royraton18@gmail.com>
2022-03-26 20:52:37 +0600

    answer the another question

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Shop from './components/Shop/Shop';
 import Props from './components/Props/Props';
+import Workreact from './components/Workreact/Workreact';
 function App() {
   return (
     <div className='page-container'>
       <Shop></Shop> 
+      <Workreact> </Workreact>
       <Props></Props>
     </div>
   );
---
+++ src/components/Workreact/Workreact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Workreact.css';
+const Workreact = () => {
+	return (
+		<div className = 'react-info-wrap'>
+			<h1 className='work-title'>The Process Of Working React </h1> 
+			<p className="react-info">
+			In my opinion, React is a javascript library So behind the sense react actually run the javascript code. React code work do like a tree and create virtual DOM and execute the code based on the js DOM. React reduce the same structure things code by the creation of component.  Once the component creates then pass the different data use props or useState or useEffect or other ways so that we create our web page efficiently and easily.  Another important thing I want to share is that react code act like water flow means it sends data only in the upper to lower direction. 
+			</p>
+		</div>
+	);
+};
+
+export default Workreact;
