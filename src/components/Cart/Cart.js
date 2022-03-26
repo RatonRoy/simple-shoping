d55a49a91f1487a426b4e85ed6d28cc2751d50ee
RatonRoy <royraton18@gmail.com>
2022-03-26 21:18:37 +0600

    add a new cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+	return (
+		<div>
+			<h3>Select One Thing</h3>	
+			<h4>Selected Items : {cart.length}</h4>
+		</div>
+	);
+};
+
+export default Cart;
